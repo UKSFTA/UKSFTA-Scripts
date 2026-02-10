38cecbddd8f7a5d7c30aee1a9b170334bb0da608
Matthew Barker <matthewbarker@librem.one>
2026-02-10 22:06:50 +0000

    fix: correct VERSION_CONFIG macro formatting for HEMTT

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -3,10 +3,15 @@
 
 #include "\x\cba\addons\main\script_macros_common.hpp"
 
-#define VERSION_CONFIG version = MAJOR.MINOR.PATCHLVL; versionStr = QUOTE(MAJOR.MINOR.PATCHLVL); versionAr[] = {MAJOR,MINOR,PATCHLVL}
+
 
 #ifdef SUBCOMPONENT
     #define COMPONENT_NAME QUOTE(PREFIX - SUBPREFIX: COMPONENT - SUBCOMPONENT)
 #else
     #define COMPONENT_NAME QUOTE(PREFIX - SUBPREFIX: COMPONENT)
 #endif
+
+#define VERSION_CONFIG \
+    version = MAJOR.MINOR; \
+    versionStr = QUOTE(MAJOR.MINOR.PATCHLVL); \
+    versionAr[] = {MAJOR,MINOR,PATCHLVL}
