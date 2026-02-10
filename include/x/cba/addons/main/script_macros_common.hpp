e1a73c4256d15f1c11168673dfe19849a1a0418c
Matthew Barker <matthewbarker@librem.one>
2026-02-10 21:59:45 +0000

    fix: include CBA headers for CI build compatibility

--- include/x/cba/addons/main/script_macros_common.hpp
+++ include/x/cba/addons/main/script_macros_common.hpp
@@ -4,12 +4,3 @@
 #define ARR_1(ARG1) ARG1
 #define ARR_2(ARG1,ARG2) ARG1, ARG2
 #define ARR_3(ARG1,ARG2,ARG3) ARG1, ARG2, ARG3
-
-// Versioning Macros
-#define VERSION "1.0.0"
-#define VERSION_STR 1.0.0
-#define VERSION_AR 1,0,0
-
-#define AUTHOR UKSF Taskforce Alpha Team
-
-#define VERSION_CONFIG version = VERSION; versionStr = QUOTE(VERSION_STR); versionAr[] = {VERSION_AR}
