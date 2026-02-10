02d8594654374cd2109e9c5cfc03c353c3e0a5ad
Matthew Barker <matthewbarker@librem.one>
2026-02-10 22:03:56 +0000

    fix: define missing VERSION_CONFIG macro

--- addons/main/script_macros.hpp
+++ addons/main/script_macros.hpp
@@ -3,6 +3,8 @@
 
 #include "\x\cba\addons\main\script_macros_common.hpp"
 
+#define VERSION_CONFIG version = MAJOR.MINOR.PATCHLVL; versionStr = QUOTE(MAJOR.MINOR.PATCHLVL); versionAr[] = {MAJOR,MINOR,PATCHLVL}
+
 #ifdef SUBCOMPONENT
     #define COMPONENT_NAME QUOTE(PREFIX - SUBPREFIX: COMPONENT - SUBCOMPONENT)
 #else
